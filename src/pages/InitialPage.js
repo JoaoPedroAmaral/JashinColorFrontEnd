475b309c6b6ed77da1fceb519a11b23119bf9aaa
joão pedro <joaopedro251900@gmail.com>
2026-01-04 16:06:57 -0300

    creating initial project

---
+++ src/pages/InitialPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function InitialPage() {
+
+    return (
+        <div>
+            <h1>Welcome to the Initial Page</h1>
+            <p>This is the starting point of the application.</p>
+        </div>
+    )
+}
